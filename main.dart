be5559c9a4960769c2323c6fc3bf75994fe29c16
Davy345294 <148599786+Davy345294@users.noreply.github.com>
2026-04-30 16:37:11 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      title: 'Executando outra tela',
-      home: Column(
+      title: 'Executando terçeira tela',
+      home: Row(
+        //dispor elemento em linha
         children: [
-          Text("Sistema Operacional"),
-          Text("Windows"),
-          Text("Linux"),
-          Text("Android"),
-          Text("IOS"),
+          Text(
+            "Flutter",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.red,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.none,
+              wordSpacing: 30, //espaçamento das palavras
+            ),
+          ),
+          Text(
+            "Dart",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.red,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.none,
+              wordSpacing: 30,
+            ),
+          ),
         ],
       ),
     ),
